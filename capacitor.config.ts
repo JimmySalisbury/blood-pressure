e4d3cd68c960de677b86d75fdaafc6c2ae4ed4b3
JimmySalisbury <james@utraconline.com>
2021-11-08 09:04:13 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'blood-pressure',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
